a6ab2f799fedf74575a1daaa8988f4b7ef2efa51
Andrew Louis <35074613+andrewvlouis@users.noreply.github.com>
2020-01-11 13:34:30 -0500

    formatted file
    missing space between new line

--- request.py
+++ request.py
@@ -1,2 +1,5 @@
-import requestsurl = 'http://localhost:5000/api'r = requests.get(url,json={'exp':1.8,})
+import requests
+
+url = 'http://localhost:5000/api'
+r = requests.get(url,json={'exp':1.8,})
 print(r.json())
